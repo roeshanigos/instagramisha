67fb26bd852c456e180a3505386150bf8b505063
Roesha Nigos <roeshanigos@fb.com>
2018-07-09 14:59:57 -0700

    Initial Commit

---
+++ instagramisha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagramisha
+//
+//  Created by Roesha Nigos on 7/9/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instagramisha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instagramisha
+//
+//  Created by Roesha Nigos on 7/9/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
